202ecc02bea1a407c9145a840110f3f3e83e7dc6
Diana Kalstein <dianakalstein@gmail.com>
2023-05-11 17:22:10 +0200

    Add Cloudinary to Techniques, also Create & Delete

--- getDOJO-backend/routes/index.routes.js
+++ getDOJO-backend/routes/index.routes.js
@@ -4,9 +4,12 @@ const router = require("express").Router();
 const Technique = require("../models/Technique.model");
 const Sensei = require("../models/Sensei.model");
 const User = require("../models/User.model");
-//const { uploader, cloudinary } = require("../config/cloudinary.config")
 const axios = require("axios");
 const { isAuthenticated } = require("../middleware/jwt.middleware");
+const fileUploader = require("../config/cloudinary.config");
+const mongoose = require("mongoose");
+
+
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
@@ -56,6 +59,56 @@ router.post("/create-profile-page/:id", (req, res, next) => {
   } else {
     res.json("Please fill in all required fields and accept the terms.");
   }
+});
+
+//Working with Cloudinary on the Techniques - Will try to implement it on the User model in case I have time left.
+
+router.post("/upload", fileUploader.single("image"), (req, res, next) => {
+ 
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+  res.json({ fileUrl: req.file.path });
+});
+ 
+router.post('/techniques', (req, res, next) => {
+ 
+ 
+  Technique.create(req.body)
+    .then(createdUserImage => {
+      res.status(200).json(createdUserImage);
+    })
+    .catch(err => next(err));
+});
+
+
+router.put("/techniques/:id", (req, res, next) => {
+  const { techniqueId } = req.params;
+
+  if (!mongoose.Types.TechniqueId.isValid(techniqueId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Technique.findByIdAndUpdate(techniqueId, req.body, { new: true })
+    .then((updatedTechnique) => res.json(updatedTechnique))
+    .catch((error) => res.json(error));
+});
+
+
+router.delete('/techniques/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  Technique.findByIdAndDelete(id)
+    .then(deletedTechnique => {
+      if (deletedTechnique) {
+        res.json({ message: `Technique with id ${id} was deleted successfully.` });
+      } else {
+        res.json({ message: `No technique found with id ${id}.` });
+      }
+    })
+    .catch(err => next(err));
 });
 
 
