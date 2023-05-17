e41b95d7a6ea5158e4b2fe89245f1963600139de
Diana Kalstein <dianakalstein@gmail.com>
2023-05-17 19:19:05 +0200

    Change techniques

--- getdojo-frontend/src/pages/Techniques.js
+++ getdojo-frontend/src/pages/Techniques.js
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import service from "../services/api.service";
+// import service from "../services/api.service";
 
 
 function Techniques() {
