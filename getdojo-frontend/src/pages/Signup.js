c3e4d98373c2222d2f1edeb358e42abb5b86cb56
Diana Kalstein <dianakalstein@gmail.com>
2023-07-13 22:22:43 +0200

    Add signup photo

--- getdojo-frontend/src/pages/Signup.js
+++ getdojo-frontend/src/pages/Signup.js
@@ -78,7 +78,7 @@ function Signup(props) {
           Login
         </button>
       </div>
-      <img src="https://res.cloudinary.com/djzhnyobz/image/upload/v1683789270/wallpaperflare.com_wallpaper_2_vzzmnw.jpg" alt="jackie-chan-signup" className="jackie-chan-signup"/>
+      <img src="https://res.cloudinary.com/iujg6ghfdf/image/upload/v1689279691/7n_shanghaidawnishappening00_fggjye.jpg" alt="jackie-chan-signup" className="jackie-chan-signup"/>
     </div>
   );
 }
