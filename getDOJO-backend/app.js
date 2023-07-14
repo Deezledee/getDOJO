7bd82b82acf0638f7374def0e2c2a21c03cb5303
Diana Kalstein <dianakalstein@gmail.com>
2023-07-14 11:10:03 +0200

    Add cors

--- getDOJO-backend/app.js
+++ getDOJO-backend/app.js
@@ -1,20 +1,33 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
 require("dotenv").config();
-const express = require("express");
 const cors = require('cors');
+
+// ℹ️ Connects to the database
 require("./db");
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require("express");
+
 const { isAuthenticated } = require("./middleware/jwt.middleware");
 
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 const app = express();
 
+// Enable CORS
 app.use(cors());
 
+require("./config")(app);
+
+// 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
 const authRouter = require("./routes/auth.routes");
 app.use("/auth", authRouter);
 
-require("./config")(app);
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
