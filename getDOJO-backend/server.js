f50b2d982571a7feb01c0db364cd7efe80148e99
Diana Kalstein <dianakalstein@gmail.com>
2026-06-17 11:40:16 +0200

    Fix backend config

--- getDOJO-backend/server.js
+++ getDOJO-backend/server.js
@@ -1,7 +1,15 @@
 const app = require("./app");
+const dbConnection = require("./db");
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server listening on http://0.0.0.0:${PORT}`);
-});
+dbConnection
+  .then(() => {
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server listening on http://0.0.0.0:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Server failed to start because MongoDB connection failed:", err);
+    process.exit(1);
+  });
